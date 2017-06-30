835cb450616de6b6a467dc644a7de77aeacceb2b
Lukasz <lukaszandruchow@gmail.com>
2017-06-30 15:13:12 +0100

    final touch of removing directory and files in it

--- app4.js
+++ app4.js
@@ -10,6 +10,7 @@ fs.unlink('writeme.txt');
 fs.rmdirSync('stuff');
 
 //------------------
+// ACT I - act of creation
 //we create a directory asynchronously - therefore we need a callback function
 fs.mkdir('stuff', function(){
 	//our callback function reads what is in the readMe.txt file and 
@@ -22,4 +23,14 @@ fs.mkdir('stuff', function(){
 		//we create a file 'writeMe.txt' in the ./stuff directory (created on line14)
 		//and populate the file with 'data' received from reading 'readMe.txt' line17
 	});
+});
+
+//--------------
+// ACT II - act of destruction
+//NOTE: you cannot remove directory UNLESS it's empty - first remove files in the dir
+//we already know how to remove files 'fs.unlink()'
+fs.unlink('./stuff/writeMe.txt', function (){
+	//right after we remove our file in the directory destined for removal,
+	//we write a callback function which will remove now empty dir
+	fs.rmdir('stuff');
 });
