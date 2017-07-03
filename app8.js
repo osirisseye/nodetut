63969cb45abaa93b570203581967919045f8e4c5
Lukasz <lukaszandruchow@gmail.com>
2017-07-03 12:50:05 +0100

    clarification on the response object

--- app8.js
+++ app8.js
@@ -27,17 +27,19 @@ myReadStream.pipe(myWriteStream);
 var http = require('http');
 var fs = require('fs');
 //Let's use pipe to send data to our users - we will recycle code from prev tutorial (app5)
+//!!Knowing that createServer takes 2 obejcts in its function - req, and res; We can write into them
 var server = http.createServer(function(req,res){
 	console.log('request was made: ' + req.url);
 	res.writeHead(200,{'Content-Type':'text/plain'});
 
 	//instead of just logging data, we reading the content of the file we specify + encode it
 	var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf-8');
-	//with our readable stream we pipe this stream into our response to the user.
+	//with our readable stream we pipe this stream into our response object to the user.
 	myReadStream.pipe(res);
 
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('yo people, now we all listen to the dj at port 3000!');
+// go to 127.0.0.1:3000 in your broswer after you type 'node app' in your temrinal to see it working
 //-----------more copmlex example ends here
